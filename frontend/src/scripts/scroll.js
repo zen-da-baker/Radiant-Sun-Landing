58f5df99544eae6fb553e98ff922de537a02f6e8
Don <120154112+zen-da-baker@users.noreply.github.com>
2025-09-02 21:49:24 -0700

    Remove frontend directory vite app

--- frontend/src/scripts/scroll.js
+++
@@ -1,7 +0,0 @@
-const backToTopBtn = document.getElementById('back-to-top')
-
-function backToTop() {
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
-}
-
-backToTopBtn.addEventListener('click', backToTop);
